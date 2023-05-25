d1d197e053aa744355843610d4fcb2fe6aa02f73
Rabin Dhukhwa <dhukhwarabin@gmail.com>
2023-05-26 08:16:24 +1000

    add filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,10 +7,25 @@ const Expenses = ({ expenses }) => {
   //directly destructuring the props object, props always comes in object to child component
   // console.log(expenses);
   const [filteredYear, setFilteredYear] = useState("2020");
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  console.log(filteredYear);
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+  let expenseContent = <p className="no_expense">No expense found</p>;
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,17 +33,26 @@ const Expenses = ({ expenses }) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+
+        {expenseContent}
       </Card>
     </div>
   );
 };
 
 export default Expenses;
+
+// alternative ways to use the same code above
+{
+  /* {filteredExpenses.length === 0 && <p>no expenses found</p>}// dynamic
+        expression
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */
+}
